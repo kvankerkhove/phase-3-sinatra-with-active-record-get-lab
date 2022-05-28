d9e63dbf693aa35e7cbb59c98db285c1a8c09666
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-05-28 10:56:37 -0400

    commit tree 3f2e3400939c3aaeadec4f27b52afb10273f21f8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < Sinatra::Base
 
+  set :default_content_type, "application/json" 
+
+  get '/bakeries' do
+    bakeries = Bakery.all
+    bakeries.to_json
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    baked_goods_by_price = BakedGood.order("price DESC")
+    baked_goods_by_price.to_json
+  end
+
+  get '/baked_goods/most_expensive' do
+    most_expensive = BakedGood.order("price DESC").first
+    most_expensive.to_json
+  end
+
   # add routes
 
 end
